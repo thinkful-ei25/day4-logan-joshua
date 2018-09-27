2252351fcb4e3b0da1841d0bbbb0213fb127a997
Logan Wang <loganxwang@hotmail.com>
2018-09-27 12:50:18 -0700

    checkuncheck

--- shoppingList/index.js
+++ shoppingList/index.js
@@ -1,10 +1,13 @@
+'use strict';
 function itemAdder(){
     
-    $('#js-shopping-list-form').submit(function(event){
-        event.preventDefault();
-        const newItem = $('.js-shopping-list-entry').val();
-        $('.shopping-list').append(
-            `<li> 
+$('#js-shopping-list-form').submit(function(event){
+    event.preventDefault();
+    const newItem = $('.js-shopping-list-entry').val();
+    //reset text box
+    event.target.reset();
+    $('.shopping-list').append(
+        `<li> 
                 <span class="shopping-item">${newItem}</span>
                 <div class="shopping-item-controls">
                     <button class="shopping-item-toggle">
@@ -15,8 +18,26 @@ function itemAdder(){
                     </button>
                 </div>
             </li>`
-        )
-        console.log(newItem);
-    })
+    )
+    
+    //console.log(newItem);
+  });
 }
+
+// Q: why does find work and not prop?
+function checkUncheckItems(){
+$('.shopping-list').on('click', '.shopping-item-toggle', function(){
+    $(this).closest('li').find('.shopping-item').toggleClass( 'shopping-item__checked');
+  });
+}
+
+function deleteItems(){
+$('shopping-list').on('click', '.shopping-item-delete',function(){
+    $(this).closest('li').remove();
+  });
+}
+
+//$( "p" ).click(function() { $( this ).toggleClass( "highlight" ); });
 $(itemAdder);
+$(checkUncheckItems);
+$(deleteItems);
