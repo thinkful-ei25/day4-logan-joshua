b125c034130cf61f949e3e9dd32e8cb08a814aaf
Joshua Adams <joshadams554@gmail.com>
2018-09-27 14:59:13 -0400

    added item add

--- shoppingList/index.js
+++ shoppingList/index.js
@@ -1,1 +1,22 @@
-console.log('Hello');
+function itemAdder(){
+    
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        const newItem = $('.js-shopping-list-entry').val();
+        $('.shopping-list').append(
+            `<li> 
+                <span class="shopping-item">${newItem}</span>
+                <div class="shopping-item-controls">
+                    <button class="shopping-item-toggle">
+                        <span class="button-label">check</span>
+                    </button>
+                    <button class="shopping-item-delete">
+                        <span class="button-label">delete</span>
+                    </button>
+                </div>
+            </li>`
+        )
+        console.log(newItem);
+    })
+}
+$(itemAdder);
